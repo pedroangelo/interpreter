e87e62e9cf9cfeac5a10b5c0b4e6eacc6991e670
Pedro Jorge Fernandes Ângelo <up201207861@fc.up.pt>
2017-01-30 19:15:19 +0000

    Add example functions Changes: 	- Examples (src/static/Examples.hs) 		- Add greatest common divisor example 		- Rename examples 		- Change arguments to example functions

--- src/static/Examples.hs
+++ src/static/Examples.hs
@@ -4,13 +4,17 @@ import Syntax
 
 lambda_I = Abstraction "i" $ Variable "i"
 
-factorial n = Application factorial' n
-factorial' = LetRec "fact" (Abstraction "n" $ If (Equal (Variable "n") (Int 0)) (Int 1) (Multiplication (Variable "n") (Application (Variable "fact") (Subtraction (Variable "n") (Int 1))))) (Variable "fact")
+factorial_func n = Application factorial (Int n)
+factorial = LetRec "fact" (Abstraction "n" $ If (Equal (Variable "n") (Int 0)) (Int 1) (Multiplication (Variable "n") (Application (Variable "fact") (Subtraction (Variable "n") (Int 1))))) (Variable "fact")
 
-power n p = Application (Application power' n) p
-power' = LetRec "power" (Abstraction "n" $ Abstraction "p" $ If (Equal (Variable "p") (Int 0)) (Int 1) (Multiplication (Variable "n") (Application (Application (Variable "power") (Variable "n")) (Subtraction (Variable "p") (Int 1))))) (Variable "power")
+power_func n p = Application (Application power (Int n)) (Int p)
+power = LetRec "power" (Abstraction "n" $ Abstraction "p" $ If (Equal (Variable "p") (Int 0)) (Int 1) (Multiplication (Variable "n") (Application (Application (Variable "power") (Variable "n")) (Subtraction (Variable "p") (Int 1))))) (Variable "power")
 
-moddiv m n = Application (Application moddiv' m) n
-moddiv' = Abstraction "m" $ Abstraction "n" $ Let "quotient" (Division (Variable "m") (Variable "n")) (Subtraction (Variable "m") (Multiplication (Variable "n") (Variable "quotient")))
+moddiv_func m n = Application (Application moddiv (Int m)) (Int n)
+moddiv = Abstraction "m" $ Abstraction "n" $ Let "quotient" (Division (Variable "m") (Variable "n")) (Subtraction (Variable "m") (Multiplication (Variable "n") (Variable "quotient")))
 
+not_func b = Application not' (Bool b)
 not' = Abstraction "b" $ If (Variable "b") (Bool False) (Bool True)
+
+gcd_func a b = Application (Application gcd' (Int a)) (Int b)
+gcd' = LetRec "gcd" (Abstraction "a" $ Abstraction "b" $ If (Equal (Variable "b") (Int 0)) (Variable "a") (Application (Application (Variable "gcd") (Variable "b")) (Application (Application moddiv (Variable "a")) (Variable "b")))) (Variable "gcd")
