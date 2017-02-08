3ebad7076d11aac2e9b0f3042f8cafeb6ca00bc1
Pedro Jorge Fernandes Ângelo <up201207861@fc.up.pt>
2017-02-08 15:37:53 +0000

    Add interpreter top level procedure New files: 	- Interpreter (src/gradual/Interpreter.hs) 		- Add interpreter top level procedure

---
+++ src/gradual/Interpreter.hs
@@ -0,0 +1,34 @@
+module Interpreter where
+
+-- Syntax & Types
+import Syntax
+import Types
+import Examples
+
+-- TypeInference
+import TypeInference
+
+-- Cast Insertion
+import CastInsertion
+
+-- Evaluation
+import Evaluation
+
+-- Imports
+import Data.Either
+
+interpret :: Expression -> IO ()
+interpret expr = do
+	let ti = inferType expr
+	if isLeft ti
+		then do
+			let (Left err) = ti
+			putStrLn err
+			return ()
+		else do
+			let (Right typ) = ti
+			putStrLn $ "Expression type: " ++ (show typ)
+			let (Right typedExpr) = insertTypeInformation expr
+			let casts = removeTypeInformation $ insertCasts typedExpr
+			let result = evaluate casts
+			putStrLn $ "Evaluation result: " ++ (show result)
