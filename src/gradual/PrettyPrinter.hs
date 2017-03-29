be45d9d1244afd55491eca6f3434b71e94f13649
Pedro Jorge Fernandes Ângelo <up201207861@fc.up.pt>
2017-03-29 15:36:50 +0100

    Add pretty printing

--- src/gradual/PrettyPrinter.hs
+++ src/gradual/PrettyPrinter.hs
@@ -1,6 +1,6 @@
 module PrettyPrinter (
-	printExpression,
-	printType
+	prettyExpression,
+	prettyType
 ) where
 
 -- Syntax & Types
@@ -9,107 +9,155 @@ import Types
 
 -- Imports
 import Data.List
+import Text.PrettyPrint.Leijen
 
-printExpression :: Expression -> String
-printExpression e@(Variable var) =
-	var
-printExpression e@(Abstraction var expr) =
-	"\\" ++ var ++ " . " ++ printExpression expr
-printExpression e@(Application expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") (" ++ printExpression expr2 ++ ")"
-printExpression e@(Ascription expr typ) =
-	"(" ++ printExpression expr ++ ") :: " ++ printType typ
-printExpression e@(Annotation var typ expr) =
-	"\\" ++ var ++ " : " ++ printType typ ++ " . " ++ printExpression expr
-printExpression e@(Bool b) = show b
-printExpression e@(Int i) = show i
-printExpression e@(Let var expr1 expr2) =
-	"let " ++ var ++ " = " ++ printExpression expr1 ++ " in " ++ printExpression expr2
-printExpression e@(Fix expr) =
-	"fix " ++ "(" ++ printExpression expr ++ ")"
-printExpression e@(LetRec var expr1 expr2) =
-	"letrec " ++ var ++ " = " ++ printExpression expr1 ++ " in " ++ printExpression expr2
-printExpression e@(If expr1 expr2 expr3) =
-	"if" ++ "(" ++ printExpression expr1 ++ ") then ("
-		++ printExpression expr2 ++ ") else (" ++ printExpression expr3 ++ ")"
-printExpression e@(Addition expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") + (" ++ printExpression expr2 ++ ")"
-printExpression e@(Subtraction expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") - (" ++ printExpression expr2 ++ ")"
-printExpression e@(Multiplication expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") * (" ++ printExpression expr2 ++ ")"
-printExpression e@(Division expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") / (" ++ printExpression expr2 ++ ")"
-printExpression e@(Equal expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") == (" ++ printExpression expr2 ++ ")"
-printExpression e@(NotEqual expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") /= (" ++ printExpression expr2 ++ ")"
-printExpression e@(LesserThan expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") < (" ++ printExpression expr2 ++ ")"
-printExpression e@(GreaterThan expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") > (" ++ printExpression expr2 ++ ")"
-printExpression e@(LesserEqualTo expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") <= (" ++ printExpression expr2 ++ ")"
-printExpression e@(GreaterEqualTo expr1 expr2) =
-	"(" ++ printExpression expr1 ++ ") >= (" ++ printExpression expr2 ++ ")"
-printExpression e@(Unit) = "()"
-printExpression e@(Pair expr1 expr2) =
-	"(" ++ printExpression expr1 ++ "," ++ printExpression expr2 ++ ")"
-printExpression e@(First expr) =
-	"(" ++ printExpression expr ++ ").1"
-printExpression e@(Second expr) =
-	"(" ++ printExpression expr ++ ").2"
-printExpression e@(Record records) =
-	"{" ++ (concat $ intersperse ", " $
-		map (\x -> (fst x) ++ " = " ++ printExpression (snd x)) records) ++ "}"
-printExpression e@(ProjectionRecord label expr typ) =
-	"(" ++ printExpression expr ++ ")." ++ label
-printExpression e@(Case expr (var1, expr1) (var2, expr2)) =
-	"case (" ++ printExpression expr ++ ") of \n  " ++
-		"inl " ++ var1 ++ " => " ++ printExpression expr1 ++ " \n| " ++
-		"inr " ++ var2 ++ " => " ++ printExpression expr2
-printExpression e@(LeftTag expr typ) =
-	"inl (" ++ printExpression expr ++ ")"
-printExpression e@(RightTag expr typ) =
-	"inr (" ++ printExpression expr ++ ")"
-printExpression e@(CaseVariant expr alternatives) =
-	"case (" ++ printExpression expr ++ ") of \n  " ++
-		(concat $ intersperse " \n| " $ map
-			(\x -> (fst3 x) ++ " " ++ (snd3 x) ++ " => " ++ printExpression (trd3 x))
-			alternatives)
-printExpression e@(Tag label expr typ) =
-	"<" ++ label ++ "=" ++ printExpression expr ++ ">"
-printExpression e@(Fold typ expr) =
-	"fold " ++ printExpression expr
-printExpression e@(Unfold typ expr) =
-	"unfold " ++ printExpression expr
-printExpression e@(TypeInformation typ expr) =
-	printExpression expr ++ " : " ++ printType typ
-printExpression e@(Cast typ1 typ2 expr) =
-	printExpression expr ++ " : " ++ printType typ1 ++ " => " ++ printType typ2
-printExpression e@(Blame typ msg) = msg
-printExpression e@(Error msg) = msg
+prettyExpression :: Expression -> Doc
+prettyExpression (Variable var) = text var
+prettyExpression (Abstraction var expr) = hcat
+	[text "\\", text var, text " . ", prettyExpression expr]
+prettyExpression (Application expr1 expr2) = hsep
+	[printParensExpression expr1, printParensExpression expr2]
+prettyExpression (Ascription expr typ) = hcat
+	[prettyExpression expr, text " :: ", prettyType typ]
+prettyExpression (Annotation var typ expr) = hcat
+	[text "\\", text var, text " : ", prettyType typ, text " . ", prettyExpression expr]
+prettyExpression (Int i) = int i
+prettyExpression (Bool b) = text $ show b
+prettyExpression (Let var expr1 expr2) = hsep
+	[text "let", text var, text "=", prettyExpression expr1, text "in"] <$$>
+	prettyExpression expr2
+prettyExpression (Fix expr) = hsep
+	[text "fix", printParensExpression expr]
+prettyExpression (LetRec var expr1 expr2) = hsep
+	[text "letrec", text var, text "=", prettyExpression expr1, text "in"] <$$>
+	prettyExpression expr2
+prettyExpression (If expr1 expr2 expr3) = nest 2 $ sep
+	[text "if" <+> prettyExpression expr1,
+	text "then" <+> prettyExpression expr2,
+	text "else" <+> prettyExpression expr3]
+prettyExpression (Addition expr1 expr2) = hsep
+	[printParensExpression expr1, text "+", printParensExpression expr2]
+prettyExpression (Subtraction expr1 expr2) = hsep
+	[printParensExpression expr1, text "-", printParensExpression expr2]
+prettyExpression (Multiplication expr1 expr2) = hsep
+	[printParensExpression expr1, text "*", printParensExpression expr2]
+prettyExpression (Division expr1 expr2) = hsep
+	[printParensExpression expr1, text "/", printParensExpression expr2]
+prettyExpression (Equal expr1 expr2) = hsep
+	[printParensExpression expr1, text "==", printParensExpression expr2]
+prettyExpression (NotEqual expr1 expr2) = hsep
+	[printParensExpression expr1, text "/=", printParensExpression expr2]
+prettyExpression (LesserThan expr1 expr2) = hsep
+	[printParensExpression expr1, text "<", printParensExpression expr2]
+prettyExpression (GreaterThan expr1 expr2) = hsep
+	[printParensExpression expr1, text ">", printParensExpression expr2]
+prettyExpression (LesserEqualTo expr1 expr2) = hsep
+	[printParensExpression expr1, text "<=", printParensExpression expr2]
+prettyExpression (GreaterEqualTo expr1 expr2) = hsep
+	[printParensExpression expr1, text ">=", printParensExpression expr2]
+prettyExpression (Unit) = text "unit"
+prettyExpression (Pair expr1 expr2) = parens $ hcat $
+	punctuate comma [prettyExpression expr1, prettyExpression expr2]
+prettyExpression (First expr) =	hcat
+	[printParensExpression expr, text ".1"]
+prettyExpression (Second expr) = hcat
+	[printParensExpression expr, text ".2"]
+prettyExpression (Tuple exprs) = parens $ hcat $
+	punctuate comma $ map prettyExpression exprs
+prettyExpression (ProjectionTuple index expr typ) = hcat
+	[printParensExpression expr, text ".", int index]
+prettyExpression (Record records) = braces $ hcat $ punctuate (comma <> space) $
+		map (\x -> text (fst x) <> text "=" <> prettyExpression (snd x)) records
+prettyExpression (ProjectionRecord label expr typ) = hcat
+	[printParensExpression expr, text ".", text label]
+prettyExpression (Case expr (var1, expr1) (var2, expr2)) = nest 2 $ vsep
+	[text "case" <+> printParensExpression expr <+> text "of",
+	text "| inl" <+> text var1 <+> text "=>" <+> prettyExpression expr1,
+	text "| inr" <+> text var2 <+> text "=>" <+> prettyExpression expr2]
+prettyExpression (LeftTag expr typ) = hsep
+	[text "inl", printParensExpression expr]
+prettyExpression (RightTag expr typ) = hsep
+	[text "inr", printParensExpression expr]
+prettyExpression (CaseVariant expr alternatives) = nest 2 $ vsep
+	[text "case" <+> printParensExpression expr <+> text "of",
+	vcat (map (\x -> hcat
+		[text "| <", text (fst3 x),	text "=", text (snd3 x), text "> => ",
+		prettyExpression (trd3 x)]) alternatives)]
+prettyExpression (Tag label expr typ) = variant $ hcat
+	[text label, text "=", prettyExpression expr]
+prettyExpression (Fold typ expr) = hsep
+	[text "fold", printParensExpression expr]
+prettyExpression (Unfold typ expr) = hsep
+	[text "unfold", printParensExpression expr]
+prettyExpression (TypeInformation typ expr) = hsep
+	[printParensExpression expr, text ":", prettyType typ]
+prettyExpression (Cast t1 t2 expr) = hsep
+	[printParensExpression expr, text ":", printParensType t1, text "=>", printParensType t2]
+prettyExpression (Blame typ msg) = hsep
+	[text "Blame:", text msg]
+prettyExpression (Error msg) = hsep
+	[text "Error:", text msg]
 
-printType :: Type -> String
-printType t@(VarType var) =
-	var
-printType t@(ArrowType t1 t2) =
-	"(" ++ printType t1 ++ ")" ++ " -> " ++ printType t2
-printType t@(IntType) = "Int"
-printType t@(BoolType) = "Bool"
-printType t@(DynType) = "?"
-printType t@(ForAll var typ) =
-	"∀" ++ var ++ "." ++ printType typ
-printType t@(UnitType) = "()"
-printType t@(ProductType t1 t2) =
-	"(" ++ printType t1 ++ " × " ++ printType t2 ++ ")"
-printType t@(RecordType ts) =
-	"{" ++ (concat $ intersperse ", " $
-		map (\x -> (fst x) ++ " : " ++ printType (snd x)) ts) ++ "}"
-printType t@(SumType t1 t2) =
-	"(" ++ printType t1 ++ " + " ++ printType t2 ++ ")"
-printType t@(VariantType ts) =
-	"<" ++ (concat $ intersperse ", " $
-		map (\x -> (fst x) ++ " : " ++ printType (snd x)) ts) ++ ">"
-printType t@(Mu var typ) =
-	"μ" ++ var ++ "." ++ printType typ
+prettyType :: Type -> Doc
+prettyType (VarType var) = text var
+prettyType (ArrowType t1 t2) = hsep
+	[printParensType t1, text "->", prettyType t2]
+prettyType (IntType) = text "Int"
+prettyType (BoolType) = text "Bool"
+prettyType (DynType) = text "?"
+prettyType (ForAll var typ) = hcat
+	[text "∀", text var, text ".", prettyType typ]
+prettyType (UnitType) = text "()"
+prettyType (ProductType t1 t2) = hsep
+	[printParensType t1, text "×", printParensType t2]
+prettyType (RecordType ts) = braces $ hcat $ punctuate (comma <> space) $
+		map (\x -> text (fst x) <> text ":" <> prettyType (snd x)) ts
+prettyType (SumType t1 t2) = hsep
+	[printParensType t1, text "+", printParensType t2]
+prettyType (VariantType ts) = variant $ hcat $ punctuate (comma <> space) $
+		map (\x -> text (fst x) <> text ":" <> prettyType (snd x)) ts
+prettyType (Mu var typ) = hcat
+	[text "μ", text var, text ".", prettyType typ]
+
+variant :: Doc -> Doc
+variant p = enclose langle rangle p
+
+printParensExpression :: Expression -> Doc
+printParensExpression expr = if needsParensExpression expr
+	then parens (prettyExpression expr)
+	else prettyExpression expr
+
+needsParensExpression :: Expression -> Bool
+needsParensExpression expr =
+	isAbstraction expr ||
+	isApplication expr ||
+	isAscription expr ||
+	isAnnotation expr ||
+	isLet expr ||
+	isFix expr ||
+	isLetRec expr ||
+	isIf expr ||
+	isArithmeticOperator expr ||
+	isRelationalOperator expr ||
+	isCase expr ||
+	isLeftTag expr ||
+	isRightTag expr ||
+	isFold expr ||
+	isUnfold expr ||
+	isTypeInformation expr ||
+	isCast expr ||
+	isBlame expr ||
+	isError expr
+
+printParensType :: Type -> Doc
+printParensType typ = if needsParensType typ
+	then parens (prettyType typ)
+	else prettyType typ
+
+needsParensType :: Type -> Bool
+needsParensType typ =
+	isArrowType typ ||
+	isForAllType typ ||
+	isProductType typ ||
+	isSumType typ ||
+	isMuType typ
